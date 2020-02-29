3c18cfce85168c39f3f4a76737438bfb57b74644
zhangsan <zhangsan@itheima>
2020-02-29 08:34:41 +0800

    病毒传播程序

---
+++ virsu_denfence/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ * @author cong
+ */
+public class Main {
+    public static void main(String[] args) {
+        //创建窗口
+        JFrame frame = new JFrame();
+        //显示窗口
+        frame.setVisible(true);
+    }
+}
